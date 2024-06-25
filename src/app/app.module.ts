1e9bd74c6b147e01b23b3d07b30bd1dc06c34e2f
Victortimbwa <victortimbwa@gmail.com>
2024-06-25 12:31:58 +0300

    Updated Registration component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,6 +24,13 @@ import { RegisterComponent } from './register/register.component';
     ReactiveFormsModule,
     MatFormFieldModule,
     BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatIconModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,28 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
 })
-export class RegisterComponent implements OnInit {
-  registerForm: FormGroup = new FormGroup({});
+export class RegisterComponent {
+  hidePassword = true;
+  hideConfirmPassword = true;
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {
-    this.registerForm = this.formBuilder.group({
-      username: ['', [Validators.required, Validators.minLength(4)]],
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
-    });
-  }
+  registrationForm = this.fb.group({
+    name: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    idNumber: ['', Validators.required],
+    password: ['', [Validators.required, Validators.minLength(8)]],
+    confirmPassword: ['', Validators.required],
+    phoneNumber: ['', Validators.required],
+    gender: ['', Validators.required],
+    dateOfBirth: ['', Validators.required],
+  });
 
-  onSubmit(): void {
-    if (this.registerForm.valid) {
-      console.log('Registration data:', this.registerForm.value);
-      // Implement your registration logic here
-    }
+  onSubmit() {
+    console.log(this.registrationForm.value);
   }
 }
